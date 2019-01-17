acfd584ac9bfbfb6928ed6a934944d082f312464
Grizzly <grizzlyxyz@gmail>
2019-01-17 09:32:02 +0100

    x

---
+++ style.js
@@ -0,0 +1,38 @@
+//
+
+let personnageX = {
+nom :"ben",
+age : 25,
+taille : "1m73",
+}
+
+let personnageY = {
+    nom : "Nicolas",
+    age : 28,
+    taille : "2m"
+}
+
+let personnageZ = {
+    nom : '',
+    age : '',
+    taille: '',
+}
+
+let personnagexyz = {
+    nom :'Maitre',
+    age : 19,
+    taille : personnageY.taille,
+    coucou: (a) =>{
+        console.log("coucou"+a);
+    }
+
+}
+
+// Remplire via la console les propriétées de personnageZ
+//Son nom doit valoir le nom de personnageX
+//Son age doit valoir celui de personnnageY 
+//Et sa taille doit être rempli avec la valeur que vous souhaitez
+
+//Exo 4
+// Via la console,lancez la methode coucou de personnagexyz qui dira "Coucou"le nom de personnagexyz
+
